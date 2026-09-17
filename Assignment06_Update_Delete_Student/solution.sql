ac5794bab443eb579f471afe121b1aa2b11856b0
dharanyapkv2004-arch <dharanyapkv2004@gmail.com>
2026-09-17 12:28:28 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,25 +1,11 @@
-DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
+UPDATE Student
+SET DepartmentID = 103
+WHERE StudentName = 'Karthik';
 
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20),
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5),
-    Email VARCHAR(30),
-    PhoneNumber BIGINT
-);
+DELETE FROM Student
+WHERE StudentID = 1002;
 
-INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
-VALUES
-(1001,'Arun','Male',101),
-(1002,'Divya','Female',102),
-(1003,'Karthik','Male',101);
+SELECT * FROM Student;
 
--- Update Karthik's DepartmentID
-
--- Delete StudentID 1002
-
--- Display all records
